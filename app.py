423b0cc6005befc5bd84a02422751b05b7732bcd
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 14:02:05 +0300

    Add comments   for logger logic

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 import logging
 
-#Instantiate the app 
+# Instantiate the app
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
-#Add logger for sanity check
+
+# Add logger for sanity check
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.warning(app.config['SQLALCHEMY_DATABASE_URI'])
 
 
 if __name__ == "__main__":
+
     app.run()
