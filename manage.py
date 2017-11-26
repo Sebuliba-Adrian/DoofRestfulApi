2fb010c789eeb0a72abdf47126d15d8870cae7c6
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-27 02:29:11 +0300

    Modify test loader

--- manage.py
+++ manage.py
@@ -18,7 +18,7 @@ manager = Manager(app)
 @manager.command
 def test():
     """Runs the unit tests without test coverage."""
-    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
