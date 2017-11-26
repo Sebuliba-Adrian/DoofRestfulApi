bc1e853e86cc99a8484bc4bf255af9356d1e3a49
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 20:32:34 +0300

    Fix configurations file

---
+++ config.py
@@ -0,0 +1,40 @@
+"""
+The script contains the configurations for the API
+
+"""
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig:
+    """Base configuration"""
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = ""
+    SECRET_KEY = 'XMLZODSHE8N6NFOZDPZA2HULWSIYJU45K6N4ZO9M'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'devdb.sqlite')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_TEST_URL') or 'sqlite:///' + os.path.join(basedir, 'testdb.sqlite')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'postgres://postgres:postgres@users-db:5432/users_prod'
+            
