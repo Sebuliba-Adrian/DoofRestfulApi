0d310e42833534e21f2c758fffa19df49989f7da
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-27 02:37:12 +0300

    Implement tests for category creation

--- tests/tests_models/test_category.py
+++ tests/tests_models/test_category.py
@@ -3,5 +3,10 @@ from models.category import CategoryModel
 from unittest import TestCase
 
 class CategoryTest(TestCase):
-    pass
+    def test_create_category(self):
+        category = CategoryModel('test')
+
+        self.assertEqual(category.name, 'test',
+                         "The name of the category after creation does not equal the constructor argument.")
+
     
