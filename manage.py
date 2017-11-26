b91c9db0ff82ea4429ad9fc41ba01813d196999e
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 20:35:34 +0300

    Add unittest module

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
+import unittest
+import coverage
 from app import app
 from db import db
+
 
 
 db.init_app(app)
@@ -11,6 +13,16 @@ db.init_app(app)
 
 # manager.add_command('db', MigrateCommand)
 manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 @manager.command
 def recreate_db():
