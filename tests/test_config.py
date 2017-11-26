90439a146629086436d4faf0f222d38581795157
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 18:54:05 +0300

    Fix a couple of imports

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,23 +1,24 @@
 import unittest
+import os
 from flask import current_app
-from flask_testing import TestCase
+from unittest import TestCase
+from config import basedir
 
-from ..app import app
+from app import app
 
 
 class TestDevelopmentConfig(TestCase):
     def create_app(self):
-        app.config.from_object('..config.DevelopmentConfig')
+        app.config.from_object('config.DevelopmentConfig')
         return app
 
     def test_app_is_development(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'XMLZODSHE8N6NFOZDPZA2HULWSIYJU45K6N4ZO9M')
+        self.assertTrue(app.config['SECRET_KEY'] ==
+                        'XMLZODSHE8N6NFOZDPZA2HULWSIYJU45K6N4ZO9M')
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
-        # self.assertTrue(
-        #     app.config['SQLALCHEMY_DATABASE_URI'] ==
-        #     'postgres://postgres:postgres@users-db:5432/users_dev'
-        # )
+        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] ==
+                        'sqlite:///' + os.path.join(basedir, 'devdb.sqlite'))
 
-        
-        
+
+                        
