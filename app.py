7b9484becd5d34d9f9445b4a573c020dea9df803
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 16:59:17 +0300

    Add pep8 formating to app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
+
 import logging
+
 
 # Instantiate the app
 app = Flask(__name__)
@@ -9,6 +11,7 @@ app.config.from_object('config.DevelopmentConfig')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.warning(app.config['SQLALCHEMY_DATABASE_URI'])
+
 
 
 if __name__ == "__main__":
