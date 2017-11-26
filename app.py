0d2a46867cafe80bed90fe16bfc70b1c356ead31
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 11:59:27 +0300

    Add logging  for sanity checking of configurations

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
+import logging
 app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logging.warning(app.config['SQLALCHEMY_DATABASE_URI'])
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
