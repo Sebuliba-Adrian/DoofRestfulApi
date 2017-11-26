7948092fd57ef83dcc2f5dceaa2b4ac518e0f659
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 12:03:14 +0300

    Add comments

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 import logging
+
+#Instantiate the app 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
+#Add logger for sanity check
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.warning(app.config['SQLALCHEMY_DATABASE_URI'])
