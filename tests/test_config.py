074921fa94a95130f997e94d90d02c43fafa310e
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 17:06:14 +0300

    Implement application factory method

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,6 +2,8 @@ import unittest
 from flask import current_app
 from flask_testing import TestCase
 
+
 class TestDevelopmentConfig(TestCase):
     def create_app(self):
-        pass
+        app.config.from_object('..config.DevelopmentConfig')
+        return app
