8c4ddde9f6872acf978f1ea76fa657340a65e48e
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 20:33:44 +0300

    Add tests for testing configurations

--- tests/test_config.py
+++ tests/test_config.py
@@ -33,9 +33,39 @@ class TestDevelopmentConfig(TestCase):
         """
         Tests that app was started with development settings
         """
-        self.assertTrue(app.config['SECRET_KEY'] ==
+        self.assertTrue(self.app.config['SECRET_KEY'] ==
                         'XMLZODSHE8N6NFOZDPZA2HULWSIYJU45K6N4ZO9M')
-        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertTrue(self.app.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
-        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] ==
+        self.assertTrue(self.app.config['SQLALCHEMY_DATABASE_URI'] ==
                         'sqlite:///' + os.path.join(basedir, 'devdb.sqlite'))
+
+
+class TestTestingConfig(TestCase):
+    """
+    Class contains test that verify the app created with
+    testing configurations
+    """
+    @staticmethod
+    def create_app():
+        app.config.from_object('config.TestingConfig')
+        return app
+
+    def setUp(self):
+        """
+        Sets up the default configurations
+        """
+        self.app = self.create_app()
+
+    def tearDown(self):
+        pass
+
+    def test_app_is_testing(self):
+        self.assertTrue(self.app.config['SECRET_KEY'] ==
+                        'XMLZODSHE8N6NFOZDPZA2HULWSIYJU45K6N4ZO9M')
+        self.assertTrue(self.app.config['DEBUG'])
+        self.assertTrue(self.app.config['TESTING'])
+        self.assertFalse(self.app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
+        self.assertTrue(
+            self.app.config['SQLALCHEMY_DATABASE_URI'] ==
+            'sqlite:///' + os.path.join(basedir, 'testdb.sqlite'))
