987b09b1cd948a8a81dbaedbbd0a7e814ee0b358
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 17:28:41 +0300

    Implement app test configuration testcase

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,7 +11,13 @@ class TestDevelopmentConfig(TestCase):
         return app
 
     def test_app_is_development(self):
-        pass
+        self.assertTrue(app.config['SECRET_KEY'] == 'XMLZODSHE8N6NFOZDPZA2HULWSIYJU45K6N4ZO9M')
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertFalse(current_app is None)
+        # self.assertTrue(
+        #     app.config['SQLALCHEMY_DATABASE_URI'] ==
+        #     'postgres://postgres:postgres@users-db:5432/users_dev'
+        # )
 
         
         
