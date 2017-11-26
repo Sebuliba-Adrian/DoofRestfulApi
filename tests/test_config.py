1392879a79d209b8bf063a744f60c5911a6d8223
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-26 17:12:30 +0300

    Add test for whether app has development configurations

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,8 +2,16 @@ import unittest
 from flask import current_app
 from flask_testing import TestCase
 
+from ..app import app
+
 
 class TestDevelopmentConfig(TestCase):
     def create_app(self):
         app.config.from_object('..config.DevelopmentConfig')
         return app
+
+    def test_app_is_development(self):
+        pass
+
+        
+        
