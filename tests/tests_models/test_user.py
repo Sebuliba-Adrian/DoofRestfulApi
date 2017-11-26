2d7d25c4aeccd808ea7f2fadc67745508c6254ec
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-27 02:45:40 +0300

    Implement test for user model

---
+++ tests/tests_models/test_user.py
@@ -0,0 +1,13 @@
+"""
+    Class contains tests for the user model
+"""
+from models.user import UserModel
+from unittest import TestCase
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = UserModel('testusername', 'testpassword')
+
+        self.assertEqual(user.username, 'testusername',"error try again" )
+        self.assertEqual(user.password, 'testpassword', "error try again")
+
