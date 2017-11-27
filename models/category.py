d060dd1873d07d2c030925d0b96b57df8784ae77
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-27 03:31:05 +0300

    Add relationship attribute to category model

--- models/category.py
+++ models/category.py
@@ -6,10 +6,12 @@ class CategoryModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+    recipes = db.relationship('RecipeModel', lazy='dynamic')
 
     def __init__(self, name):
         self.name = name
-
+    
+    
     
 
     @classmethod
