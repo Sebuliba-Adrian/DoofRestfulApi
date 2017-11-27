5696054508a022e559c5e6c789f6bb6961ed5768
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-27 20:44:08 +0300

    Test for creating a category

--- tests/tests_resources/test_category.py
+++ tests/tests_resources/test_category.py
@@ -1,4 +1,20 @@
+import json
 from tests.base import BaseTestCase
+from models.category import CategoryModel
+
 
 class CategoryTest(BaseTestCase):
-    pass
+    """Ensures that the category resource functions as expectected"""
+
+    def test_create_category(self):
+        """Ensure that a recipe category is created """
+
+        with self.app() as client:
+            with self.app_context():
+                resp = client.post('/category/somerecipecategory')
+
+                self.assertEqual(resp.status_code, 201)
+                self.assertIsNotNone(
+                    CategoryModel.find_by_name('somerecipecategory'))
+                self.assertDictEqual({'id': 1, 'name': 'somerecipecategory', 'recipies': []},
+                                     json.loads(resp.data))
