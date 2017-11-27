fc3bbee40312e1e56095506a0e7a83ec3b363722
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-27 16:32:15 +0300

    Add pep8 formatting

--- tests/tests_resources/test_user.py
+++ tests/tests_resources/test_user.py
@@ -1,28 +1,33 @@
 from models.user import UserModel
-from  unittest import TestCase
+from unittest import TestCase
 from tests.base import BaseTestCase
 import json
 
+
 class UserResourceTest(BaseTestCase):
     """Ensure a new user resource can be added to the database."""
+
     def test_register_user(self):
         """Ensure that user resource  is registered"""
         with self.app() as client:
             with self.app_context():
-                response = client.post('/register', data={'username': 'testusername', 'password': 'testpassword'})
+                response = client.post(
+                    '/register', data={'username': 'testusername', 'password': 'testpassword'})
 
                 self.assertEqual(response.status_code, 201)
-                self.assertIsNotNone(UserModel.find_by_username('testusername'))
+                self.assertIsNotNone(
+                    UserModel.find_by_username('testusername'))
                 self.assertDictEqual({'message': 'User created successfully.'},
                                      json.loads(response.data))
-            
-    
 
     def test_register_duplicate_user(self):
+        """Ensure that user resource registered is not a duplicate"""
         with self.app() as client:
             with self.app_context():
-                client.post('/register', data={'username': 'testusername', 'password': 'testpassword'})
-                response = client.post('/register', data={'username': 'testusername', 'password': 'testpassword'})
+                client.post(
+                    '/register', data={'username': 'testusername', 'password': 'testpassword'})
+                response = client.post(
+                    '/register', data={'username': 'testusername', 'password': 'testpassword'})
 
                 self.assertEqual(response.status_code, 400)
                 self.assertDictEqual({'message': 'A user with that username already exists'},
