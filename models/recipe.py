4d3e246863268e0fb1426a8c843d4ef468c4c0a5
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-27 03:22:54 +0300

    Implement recipes model to meet the test

---
+++ models/recipe.py
@@ -0,0 +1,25 @@
+from db import db
+
+
+class RecipeModel(db.Model):
+    __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(800))
+
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship('CategoryModel')
+
+    def __init__(self, name, description, category_id):
+        self.name = name
+        self.description = description
+        self.category_id = category_id
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
