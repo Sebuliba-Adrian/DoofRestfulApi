15c9fe67efa8dd301e5be010f91a97dcf6ecd33d
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-28 20:10:01 +0300

    Fix misplaced tests

---
+++ tests/tests_utilities/test_user.py
@@ -0,0 +1,17 @@
+
+from tests.base import BaseTestCase
+from models.user import UserModel
+
+
+class UserTest(BaseTestCase):
+    def test_crud(self):
+        with self.app_context():
+            user = UserModel('testusername', 'testpassword')
+
+            self.assertIsNone(UserModel.find_by_username('testusername'))
+            self.assertIsNone(UserModel.find_by_id(1))
+
+            user.save_to_db()
+
+            self.assertIsNotNone(UserModel.find_by_username('testusername'))
+            self.assertIsNotNone(UserModel.find_by_id(1))
