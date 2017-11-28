67f1cc7f52387c36850f63fc4eb4839ad54d22dd
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-28 18:07:29 +0300

    Add method to delete a recipe from the database

--- models/recipe.py
+++ models/recipe.py
@@ -26,3 +26,7 @@ class RecipeModel(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
