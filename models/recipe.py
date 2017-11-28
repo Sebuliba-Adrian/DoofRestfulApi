0d7d882c25229950ad1c9d8b2a2cd26d1ac55332
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-28 19:10:50 +0300

    Add pep8 formatting

--- models/recipe.py
+++ models/recipe.py
@@ -2,6 +2,7 @@ from db import db
 
 
 class RecipeModel(db.Model):
+    """This is a recipe model class"""
     __tablename__ = 'recipes'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -17,16 +18,20 @@ class RecipeModel(db.Model):
         self.category_id = category_id
 
     def json(self):
+        """This method jsonifies the recipe model"""
         return {'name': self.name, 'description': self.description}
 
     @classmethod
     def find_by_name(cls, name):
+        """This class method returns the recipe by name"""
         return cls.query.filter_by(name=name).first()
 
     def save_to_db(self):
+        """This method  saves recipe to the database"""
         db.session.add(self)
         db.session.commit()
 
     def delete_from_db(self):
+        """This method deletes recipe from the database"""
         db.session.delete(self)
         db.session.commit()
