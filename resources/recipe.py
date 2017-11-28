be03b2560a7f2df9f02a11d590dba08f07ad8441
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-11-28 15:18:00 +0300

    Fix error in token generation for tests

--- resources/recipe.py
+++ resources/recipe.py
@@ -1,13 +1,27 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
 from models.recipe import RecipeModel
 
 
 class Recipe(Resource):
     """This class represents a Recipe resource  """
+    parser = reqparse.RequestParser()
+    parser.add_argument('description',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!")
+    parser.add_argument('category_id',
+                        type=int,
+                        required=True,
+                        help="Every recipe needs a category id.")
 
+    @jwt_required()
     def get(self, name):
         """This method handles requests for getting a recipe by name"""
-        pass
+        recipe = RecipeModel.find_by_name(name)
+        if recipe:
+            return recipe.json()
+        return {'message': 'Recipe not found'}, 404
 
     def post(self, name):
         """This method handles requests for adding recipe to storage by name"""
