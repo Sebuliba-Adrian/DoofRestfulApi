3b400990f34676b6bcca1b82aa9a53eddaeaeac1
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-12-10 11:37:20 +0300

    Import flask-jwt-extented library

--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,10 @@
+from flask import jsonify, request, make_response
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+
 from models.user import UserModel
 
 
@@ -29,3 +35,44 @@ class UserRegister(Resource):
         return {'message': 'User created successfully.'}, 201
 
 
+class UserLogin(Resource):
+    """
+    This resource allows users to login by sending a
+    POST request with their username and password.
+    """
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank.")
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank.")
+
+    def post(self):
+
+        data = UserLogin.parser.parse_args()
+
+        try:
+
+            user = UserModel.find_by_username(data['username'])
+            if not request.is_json:
+                return {"message": "Missing JSON in request"}, 400
+
+            username = data['username']
+            password = data['password']
+            if not username:
+                return {"message": "Missing username parameter"}, 400
+            if not password:
+                return {"message": "Missing password parameter"}, 400
+            if (user and user.username and user.password):
+                # Identity can be any data that is json serializable
+                access_token = create_access_token(identity=username)
+                return {'access_token': str(access_token)}, 201
+            else:
+                return {"message": "Bad username or password"}, 401
+
+        except Exception as e:
+            response = {'message': str(e)}
+            return response, 500
