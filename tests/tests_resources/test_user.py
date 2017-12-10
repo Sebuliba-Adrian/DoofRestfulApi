5b8fec5f59f015c9002cd5bd719e6cc3349e597e
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-12-10 11:33:01 +0300

    Add tests for login functionality

--- tests/tests_resources/test_user.py
+++ tests/tests_resources/test_user.py
@@ -46,3 +46,61 @@ class UserResourceTest(BaseTestCase):
 
                 self.assertIn('access_token', json.loads(
                     auth_response.data).keys())
+
+    def test_missing_username_at_login(self):
+        """Ensure that username is not missing at login"""
+        with self.app() as client:
+            with self.app_context():
+                auth_response = client.post('/login',
+                                              data=json.dumps(
+                                                  {'username': '', 'password': 'testpassword'}),
+                                              headers={'Content-Type': 'application/json'})
+
+                self.assertEqual(auth_response.status_code, 400)
+                self.assertDictEqual({'message': 'Missing username parameter'},
+                                     json.loads(auth_response.data))
+
+    def test_missing_password_at_login(self):
+        """Ensure that password is not missing at login"""
+        with self.app() as client:
+            with self.app_context():
+                auth_response = client.post('/login',
+                                              data=json.dumps(
+                                                  {'username': 'testusername', 'password': ''}),
+                                              headers={'Content-Type': 'application/json'})
+
+                self.assertEqual(auth_response.status_code, 400)
+                self.assertDictEqual({'message': 'Missing password parameter'},
+                                     json.loads(auth_response.data))
+
+    def test_wrong_credentials_at_login(self):
+        """Ensure that the username and password are not wrong"""
+        with self.app() as client:
+            with self.app_context():
+                auth_response = client.post('/login',
+                                              data=json.dumps(
+                                                  {'username': 'wrongusername', 'password': 'wrongpassword'}),
+                                              headers={'Content-Type': 'application/json'})
+
+                self.assertEqual(auth_response.status_code, 401)
+                self.assertDictEqual({'message': 'Bad username or password'},
+                                     json.loads(auth_response.data))
+
+    def test_login_non_json_input(self):
+        """Ensure that only json input is taken in"""
+        with self.app() as client:
+            with self.app_context():
+                response = client.post('/login', data=self.non_json_data,
+                                         content_type='application/json')
+                self.assertEqual(response.status_code, 400)
+
+    def test_login_invalid_json_keys(self):
+        """Ensure that json keys are not invalid"""
+        with self.app() as client:
+            with self.app_context():
+                response = client.post('/login',
+                                         data=json.dumps(self.wrong_keys_data),
+                                         content_type='application/json')
+                self.assertEqual(response.status_code, 400)
+
+                
