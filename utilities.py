f28def0c845069a7cfdc6fb686c7bfe5c8d9425f
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-12-10 23:54:33 +0300

    Add  utilities.py for pagination

---
+++ utilities.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, abort, request, jsonify
+
+
+def get_paginated_list(table, url, start, limit):
+    # check if page exists
+    results = table.query.all()
+    count = len(results)
+    if (count < start):
+        abort(404)
+    # make response
+    obj = {}
+    obj['start'] = start
+    obj['limit'] = limit
+    obj['count'] = count
+    # make URLs
+    # make previous url
+    if start == 1:
+        obj['previous'] = ''
+    else:
+        start_copy = max(1, start - limit)
+        limit_copy = start - 1
+        obj['previous'] = url + '?start=%d&limit=%d' % (start_copy, limit_copy)
+    # make next url
+    if start + limit > count:
+        obj['next'] = ''
+    else:
+        start_copy = start + limit
+        obj['next'] = url + '?start=%d&limit=%d' % (start_copy, limit)
+    # finally extract result according to bounds
+    obj['results'] = results[(start - 1):(start - 1 + limit)]
+    return obj
