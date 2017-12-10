b76bf97ff39bfbe59bd462ff17fab99e6ac89b1e
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-12-10 11:30:03 +0300

    Modify manage.py file

--- manage.py
+++ manage.py
@@ -3,13 +3,13 @@ Script contains commands that can be called from the command line
 to a Manager instance, for the flask application
 """
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import MigrateCommand
 import unittest
 
 from app import app
 from db import db
 
-
+app.config.from_object('config.DevelopmentConfig')
 db.init_app(app)
 
 manager = Manager(app)
@@ -25,10 +25,16 @@ def test():
     return 1
 
 
-migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == '__main__':
