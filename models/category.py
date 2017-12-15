6427541c1f32a8720568dd9c65d3f4836530abe1
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-12-15 14:43:27 +0300

    Fix table relationships

--- models/category.py
+++ models/category.py
@@ -8,7 +8,7 @@ class CategoryModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    recipes = db.relationship('RecipeModel',backref='categorymodel',lazy='dynamic')
+    recipes = db.relationship('RecipeModel', lazy='dynamic')
 
     def __init__(self, name):
         self.name = name
@@ -25,15 +25,16 @@ class CategoryModel(db.Model):
         """This class method queries the database and returns the category model by name"""
         return cls.query.filter_by(name=name).first()
 
+    @classmethod
+    def find_by_id(cls, id):
+        """This class method queries the database and returns the category model by id"""
+        return cls.query.filter_by(id=id).first()
+
+
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
-    @staticmethod
-    def save_all_db(categories):
-        db.session.add_all(categories)
-        db.session.commit()
-
     def delete_from_db(self):
         db.session.delete(self)
-        db.session.commit()
+        return db.session.commit()
