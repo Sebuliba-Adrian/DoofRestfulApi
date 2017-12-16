351a6a78f930ccefbdf20b6d9168a48baa034f7d
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-12-16 16:52:08 +0300

    Add parsers

--- parsers.py
+++ parsers.py
@@ -1,4 +1,6 @@
 from flask_restful import reqparse
+
+# Parser for category put request
 category_put_parser = reqparse.RequestParser()
 category_put_parser.add_argument(
     'name',
@@ -6,3 +8,43 @@ category_put_parser.add_argument(
     required=True,
     help="This field cannot be left blank!"
 )
+
+# Parser for recipes put request
+recipe_put_parser = reqparse.RequestParser()
+recipe_put_parser.add_argument('name',
+                               type=str,
+                               required=True,
+                               help="This field cannot be left blank!")
+
+recipe_put_parser.add_argument('description',
+                               type=str,
+                               required=True,
+                               help="This field cannot be left blank!")
+recipe_put_parser.add_argument('category_id',
+                               type=int,
+                               required=True,
+                               help="Every recipe needs a category id.")
+
+# Parser for recipes post request
+recipe_post_parser = reqparse.RequestParser()
+recipe_post_parser.add_argument('name',
+                                type=str,
+                                required=True,
+                                help="This field cannot be left blank!")
+
+recipe_post_parser.add_argument('description',
+                                type=str,
+                                required=True,
+                                help="This field cannot be left blank!")
+recipe_post_parser.add_argument('category_id',
+                                type=int,
+                                required=True,
+                                help="Every recipe needs a category id.")
+
+
+# Parser for categories post request
+category_post_parser = reqparse.RequestParser()
+category_post_parser.add_argument('name',
+                                  type=str,
+                                  required=True,
+                                  help="This field cannot be left blank!")
