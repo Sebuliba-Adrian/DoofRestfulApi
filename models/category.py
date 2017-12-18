6bd9d7f5b5495f72b11c83286db81802b3cbe024
sebuliba-adrian <adrian.sebuliba1@gmail.com>
2017-12-18 09:39:51 +0300

    Add pep8

--- models/category.py
+++ models/category.py
@@ -9,6 +9,7 @@ class CategoryModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     recipes = db.relationship('RecipeModel', lazy='dynamic')
+    
 
     def __init__(self, name):
         self.name = name
