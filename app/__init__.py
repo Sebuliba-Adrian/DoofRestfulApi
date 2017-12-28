e201d51f56e05c2276ba263d4bedc8413b900351
adrian <adrian.sebuliba1@gmail.com>
2017-12-28 18:09:28 +0300

    Refactor package constructor

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flasgger import Swagger
 
 from flask_jwt_extended import JWTManager, get_jwt_identity
 from flask_restful import Api
-from .resources.category import Category, CategoryList
+# from .resources.category import Category, CategoryList
 from .resources.recipe import Recipe, RecipeList
 from .resources.user import PasswordReset, UserLogin, UserRegister
 
