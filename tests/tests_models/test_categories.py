5e8bd40abc42c2a1d0e752d0ae6d75e987c1c790
adrian <adrian.sebuliba1@gmail.com>
2017-12-29 13:06:24 +0300

    Add tests for category models

---
+++ tests/tests_models/test_categories.py
@@ -0,0 +1,15 @@
+from app.models import CategoryModel
+from app.models import UserModel
+
+from unittest import TestCase
+
+class CategoryTest(TestCase):
+    def test_create_category(self):
+        user= UserModel(username='testusername',password='testpassword')
+        
+        category = CategoryModel(name='categoryname', user=user)
+
+        self.assertEqual(category.name, 'categoryname',
+                         "The name of the category after creation in wrong!!.")
+
+    
