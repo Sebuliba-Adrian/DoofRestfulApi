da384f9d76e3644b1ddfe4a894d377b8ccc58b94
adrian <adrian.sebuliba1@gmail.com>
2018-01-02 11:10:28 +0300

    Add more  helper functions to utilities file

--- app/utilities.py
+++ app/utilities.py
@@ -1,8 +1,32 @@
-from app.models import CategoryModel
+from app.models import CategoryModel, RecipeModel
 
-def search_categories(q):
+
+def search_categories(q, user_id):
     '''
     method that gets data based on the search query provided
     '''
-    categories = CategoryModel.query.filter(CategoryModel.name.contains(q)).all()
+    categories = CategoryModel.query.filter(CategoryModel.created_by == user_id,
+                                            CategoryModel.name.contains(q))
+
     return categories
+
+
+def search_recipes(q):
+    '''
+    method that gets data based on the search query provided
+    '''
+
+    recipes = RecipeModel.query.filter(RecipeModel.name.contains(q)).all()
+    return recipes
+
+
+def user_name_validator(value):
+    pass
+
+
+def recipe_validator(value):
+    pass
+
+
+def category_validator(value):
+    pass
